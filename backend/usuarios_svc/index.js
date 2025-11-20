a4667d776337c93d8437b3347d53f31b34d9a56f
dioni <dioni@dioni.>
2025-11-20 18:56:46 -0300

    usuarios_svc

---
+++ backend/usuarios_svc/index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const fs = require('fs');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+const filePath = path.join(__dirname, 'db', 'usuarios.json');
+
+function load_usuarios() {
+  const data = fs.readFileSync(filePath);
+  return JSON.parse(data);
+} 
+
+function save_usuarios(usuario) {
+  fs.writeFileSync(filePath, JSON.stringify(usuario, null, 2));
+}
+
+app.get('/usuarios/', (req, res) => {
+  const usuarios = load_usuarios();
+  res.json(usuarios);
+});
+
+
+app.get('/usuarios/:id', (req, res) => {
+  const usuarios = load_usuarios();
+  const usuario = usuarios.find(c => c.id === Number(req.params.id));
+  if (!usuario) return res.status(404).json({ erro: 'Não encontrado' });
+  res.json();
+  res.json(usuario);
+});
+
+app.post('/usuarios', async (req, res) =>{
+  const { nome, cpf_cnpj, email, celular, endereco, cargo } = req.body;
+    try {
+      const usuarios = load_usuarios();
+      const novo = {id : get_next_id(db_path), nome, descricaoc, cpf_cnpj, email, celular, endereco, cargo};
+      usuarios.push(novo);
+      save_usuarios(usuarios);
+  
+      res.status(201).json(novo);
+    } catch {
+      res.status(400).json({ erro: 'Dados inválidos' });
+    }
+});
+
+
+app.put('/usuarios/:id', (req, res) => {
+  const usuarios = load_usuarios();
+  const index = usuarios.findIndex(c => c.id === Number(req.params.id));
+  if (index === -1) return res.status(404).json({ erro: 'Não encontrada' });
+
+  usuarios[index] = { id: Number(req.params.id), ...req.body };
+  save_usuarios(usuarios);
+
+  res.json(usuarios[index]);
+});
+
+app.patch('/usuarios/:id', (req, res) => {
+  const usuarios = load_usuarios();
+  const usuario = usuarios.find(c => c.id === Number(req.params.id));
+  if (!usuario) return res.status(404).json({ erro: 'Não encontrada' });
+
+  Object.assign(usuario, req.body);
+  save_usuarios(usuarios);
+
+  res.json(usuario);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+  const usuarios = load_usuarios();
+  const index = usuarios.findIndex(c => c.id === Number(req.params.id));
+  if (index === -1) return res.status(404).json({ erro: 'Não encontrada' });
+
+  usuarios.splice(index, 1);
+  save_usuarios(usuarios);
+
+  res.status(204).send();
+});
+
+
+app.head('/usuarios/:id', (req, res) => {
+  const usuarios = load_usuarios();
+  const exists = usuarios.some(c => c.id === Number(req.params.id));
+  res.sendStatus(exists ? 200 : 404);
+});
+
+app.options('/usuarios', (req, res) => {
+  res.set('Allow', 'GET,POST,PUT,PATCH,DELETE,OPTIONS,HEAD');
+  res.send();
+});
+
+const doc = yaml.load(fs.readFileSync('../openapi.yaml', 'utf8'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(doc));
+
+app.listen(PORT, () => {
+  console.log(`usuarios_svc rodando em http://localhost:${PORT}`);
+});
+
