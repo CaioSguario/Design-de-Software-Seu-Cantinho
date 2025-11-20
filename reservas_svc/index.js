470f37f6edbdadc2f42e48974c927b8e885333a0
dioni <dioni@dioni.>
2025-11-20 17:26:29 -0300

    imports e db

--- reservas_svc/index.js
+++ reservas_svc/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+const filePath = path.join(__dirname, 'db', 'reservas.json');
 
 function loadReservas() {
   const data = fs.readFileSync(filePath);
