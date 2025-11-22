c55b84cbeac0123cbc9ab728bd8e409f7ebc64ac
João Eduardo Zangari Ambrosio <joaozangari21@gmail.com>
2025-11-22 13:18:23 -0300

    validacao

--- backend/reservas_svc/index.js
+++ backend/reservas_svc/index.js
@@ -16,11 +16,33 @@ function load_reservas() {
   check_bd(db_dir, db_path);
   const data = fs.readFileSync(db_path);
   return JSON.parse(data);
-} 
+}
 
 function save_reservas(consultas) {
   check_bd(db_dir, db_path);
   fs.writeFileSync(db_path, JSON.stringify(consultas, null, 2));
+}
+
+function validate_reserva(res, espaco_id, self_id, inicio, fim, reservas) {
+
+  if (inicio >= fim) {
+    res.status(400).json({ erro: "Intervalo de datas inválido" });
+    return false;
+  }
+
+  // Checa se existe concomitancia
+  const conflito = reservas.some(r =>
+    r.id !== self_id &&
+    r.espaco_id === espaco_id &&
+    new Date(r.data_inicio).getTime() < fim &&
+    new Date(r.data_fim).getTime() > inicio
+  );
+
+  if (conflito) {
+    res.status(409).json({ erro: "Conflito: espaço já reservado neste horário" });
+    return false;
+  }
+  return true;
 }
 
 app.get('/reservas/', (req, res) => {
@@ -36,21 +58,29 @@ app.get('/reservas/:id', (req, res) => {
   res.json(reserva);
 });
 
-app.post('/reservas', async (req, res) =>{
+app.post('/reservas', async (req, res) => {
   const { espaco_id, cliente_id, data_inicio, data_fim, status, status_pagamento, preco_total } = req.body;
-    try {
-      const resposta = await axios.get(`http://localhost:3001/espacos/${espaco_id}`);
-      const espaco = resposta.data;
-  
-      const reservas = load_reservas();
-      const nova = { id : get_next_id(db_path), espaco_id, cliente_id, data_inicio, data_fim, status, status_pagamento, preco_total, criado_em : Date.now()};
-      reservas.push(nova);
-      save_reservas(reservas);
-  
-      res.status(201).json(nova);
-    } catch {
-      res.status(400).json({ erro: 'Espaço inválido' });
-    }
+
+  const inicio = new Date(data_inicio).getTime();
+  const fim = new Date(data_fim).getTime();
+
+  try {
+    const resposta = await axios.get(`http://localhost:3001/espacos/${espaco_id}`);
+
+    const reservas = load_reservas();
+
+    const valid = validate_reserva(res, espaco_id, null, inicio, fim, reservas);
+    if(!valid) return;
+
+    const nova = { id: get_next_id(db_path), espaco_id, cliente_id, data_inicio, data_fim, status, status_pagamento, preco_total, criado_em: Date.now() };
+
+    reservas.push(nova);
+    save_reservas(reservas);
+
+    res.status(201).json(nova);
+  } catch {
+    res.status(400).json({ erro: 'Espaço inválido' });
+  }
 });
 
 
@@ -58,6 +88,15 @@ app.put('/reservas/:id', (req, res) => {
   const reservas = load_reservas();
   const index = reservas.findIndex(c => c.id === Number(req.params.id));
   if (index === -1) return res.status(404).json({ erro: 'Não encontrada' });
+
+  const { espaco_id, data_inicio, data_fim } = req.body;
+  const id = Number(req.params.id);
+  
+  const inicio = new Date(data_inicio).getTime();
+  const fim = new Date(data_fim).getTime();
+
+  const valid = validate_reserva(res, espaco_id, id, inicio, fim, reservas);
+  if(!valid) return;
 
   reservas[index] = { id: Number(req.params.id), ...req.body };
   save_reservas(reservas);
@@ -69,6 +108,15 @@ app.patch('/reservas/:id', (req, res) => {
   const reservas = load_reservas();
   const reserva = reservas.find(c => c.id === Number(req.params.id));
   if (!reserva) return res.status(404).json({ erro: 'Não encontrada' });
+
+  const { espaco_id, data_inicio, data_fim } = req.body;
+  const id = Number(req.params.id);
+
+  const inicio = new Date(data_inicio).getTime();
+  const fim = new Date(data_fim).getTime();
+
+  const valid = validate_reserva(res, espaco_id, id, inicio, fim, reservas);
+  if(!valid) return;
 
   Object.assign(reserva, req.body);
   save_reservas(reservas);
