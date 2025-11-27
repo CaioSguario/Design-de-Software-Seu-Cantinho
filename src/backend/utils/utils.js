9a455231aff961270e2fcf415204cce2a515adbb
Caio Sguario <cfssguario@gmail.com>
2025-11-26 22:55:26 -0300

    Fix: get_next_id consertada e post do espacos consertado. espacos funcionando

--- src/backend/utils/utils.js
+++ src/backend/utils/utils.js
@@ -6,26 +6,37 @@ function get_next_id(db_path) {
     return 1;
   }
   const raw = fs.readFileSync(db_path, 'utf8');
-  if (!raw.trim()) {
+  if (!raw.trim() || raw.trim() === '[]') {
     return 1;
   }
-  const data = JSON.parse(raw);
-  if (!Array.isArray(data) || data.length === 0) {
-    return 1;
-  }
-  const last = data[data.length - 1].id;
+  
+  try {
+    const data = JSON.parse(raw);
+    if (!Array.isArray(data) || data.length === 0) {
+      return 1;
+    }
 
-  return last + 1;
+    const maxId = data.reduce((max, obj) => {
+        const currentId = obj.id || 0; 
+        return currentId > max ? currentId : max;
+    }, 0); 
+    
+    return maxId + 1;
+
+  } catch (e) {
+    console.error("Erro ao fazer parse do DB, resetando ID count.");
+    return 1;
+  }
 }
 
 function check_bd(db_dir, db_path) {
   if (!fs.existsSync(db_dir))
-    fs.mkdirSync(db_dir,)
+    fs.mkdirSync(db_dir, { recursive: true });
+    
   if (!fs.existsSync(db_path)) {
-    fs.writeFileSync(db_path, JSON.stringify({ messages: [] }, null, 2));
+    fs.writeFileSync(db_path, '[]');
   }
 }
-
 
 module.exports = {
   get_next_id,
